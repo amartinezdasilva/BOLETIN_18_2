8e722c59cd15e362a5c5eb385e516fe3259f6618
AAron <AAron@Cobo-PC>
2016-03-12 12:01:49 +0100

    commit realizado

---
+++ src/boletin18_2/Boletin18_2.java
@@ -0,0 +1,15 @@
+
+package boletin18_2;
+
+
+public class Boletin18_2 {
+
+
+    public static void main(String[] args) {
+     Metodos obx = new Metodos();
+     obx.cargarArray();
+     obx.aprobadosysuspensos();
+     obx.notaMedia();
+     obx.notaAlta();
+    }
+}
